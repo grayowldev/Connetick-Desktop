4c3e264e2d12904b4ecc5e36abfaa4c80c88eed0
Kwasi <truegrayowl@gmail.com>
2017-04-18 17:16:13 -0400

    Added Scene Builder

---
+++ TicketLog/src/Logger.java
@@ -0,0 +1,18 @@
+/**
+ * Created by kwasi on 4/18/2017.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Logger extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}

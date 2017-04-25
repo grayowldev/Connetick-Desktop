e036593b659c52c3e9714979d8fca7f34439e4ab
Kwasi <truegrayowl@gmail.com>
2017-04-25 17:58:03 -0400

    server edit

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,9 +1,16 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by kwasi on 4/18/2017.
@@ -12,6 +19,8 @@ public class Server extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    private int clientNo = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -22,5 +31,23 @@ public class Server extends Application {
         primaryStage.setTitle("Connetick Seer");
         primaryStage.show();
         primaryStage.setIconified(true);
+
+        new Thread(() -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(3000);
+                Platform.runLater(() ->{
+
+                });
+
+                Socket socket = serverSocket.accept();
+
+                DataInputStream clientInput = new DataInputStream(socket.getInputStream());
+                DataOutputStream clientOutput = new DataOutputStream(socket.getOutputStream());
+
+            }
+            catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }).start();
     }
 }
